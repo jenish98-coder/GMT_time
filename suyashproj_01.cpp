72caf965afe24a0355881cbf7bb34e9f89e8b4f4
jenish98-coder <125825465+jenish98-coder@users.noreply.github.com>
2023-09-03 19:32:55 +0545

    Add files via upload

---
+++ suyashproj_01.cpp
@@ -0,0 +1,24 @@
+/*Write a program that takes time input (hour and minute) in -4:00 GMT and converts 
+the time into equivalent +5:45 GMT time
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int t1,t2,h;
+    float m;
+    cout<<"Hour"<<"=";
+    cin>>h;
+    cout<<"Minute"<<"=";
+    cin>>m;
+
+    cout<<"Entered Time"<<"= -"<<h<<":"<<m<<"GMT"<<endl;
+   
+
+    int c=h+13;
+    int d=m+45;
+
+    cout<<"Converted Time"<<"= +"<<c<<":"<<d<<"GMT";
+
+ return 0;
+
+}
